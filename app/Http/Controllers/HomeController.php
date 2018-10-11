4ea285e16f6857891fbdc2ca98b794c68599e6c2
daniel fernando betancur villada <danielpcpx@hotmail.com>
2018-10-11 09:33:34 -0500

    Se crea todo el sistema de roles de usuario

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,22 +7,24 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * Show the application dashboard.
      *
-     * @return void
+     * @return \Illuminate\Http\Response
      */
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function index(Request $request)
     {
+        $request->user()->authorizeRoles(['user', 'admin']);
         return view('home');
     }
+    /*
+    public function someAdminStuff(Request $request)
+    {
+        $request->user()->authorizeRoles(‘admin’);
+        return view(‘some.view’);
+    }
+    */
 }
