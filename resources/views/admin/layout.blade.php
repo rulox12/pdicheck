38e8c3245fd9a86291b08f958eff3bfb409d7a3e
Esleider Jaramillo <devesleider@gmail.com>
2018-11-08 16:57:45 -0500

    Corrección de cista update

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,7 +3,7 @@
 This is a starter template page. Use this page to start your new project from
 scratch. This page gets rid of all links and provides the needed markup only.
 -->
-<html>
+<html lang="es">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
--- resources/views/implementation/index.blade.php
+++ resources/views/implementation/index.blade.php
@@ -27,10 +27,11 @@
             <td>{{ $item->name_leader }}</td>
             <td>{{ $item->name_engineer }}</td>
             <td class="progress progress-sm active">
-              <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: {{ $implementation[0]->progress }}%; height: 100%; color: black">
-                <b>{{ $implementation[0]->progress }}%</b>
-                <span class="sr-only">40% Complete (success)</span>
-              </div>
+            <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: {{ $item->progress }}%; color: black">
+              <b>{{ $item->progress }}%</b>
+              <span class="sr-only">40% Complete (success)</span>
+
+            </div>
             </td>
           </tr>
         @endforeach
