e2fcdc1716b72f1020bf2160657312154eeba3a2
daniel fernando betancur villada <danielpcpx@hotmail.com>
2018-11-21 14:06:22 -0500

    l

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -31,37 +31,50 @@ scratch. This page gets rid of all links and provides the needed markup only.
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 @auth
-  @include('sweetalert::alert')
-  <div class="wrapper">
+@include('sweetalert::alert')
+<div class="wrapper">
 
-    <!-- Main Header -->
-    <header class="main-header">
+  <!-- Main Header -->
+  <header class="main-header">
 
-      <!-- Logo -->
-      <a href="#" class="logo">
-        <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>Implementacion</b></span>
-        <!-- logo for regular state and mobile devices -->
-        <span class="logo-lg"><b>Implementacion</b></span>
+    <!-- Logo -->
+    <a href="index2.html" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini"><b>P</b>DI</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><b>Implementacion</b></span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+        <span class="sr-only">Toggle navigation</span>
       </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="{{ asset('adminlte/img/user1.png') }}" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs">
+                @auth
+                  {{ auth()->user()->name}}
+                @else
+                  no esta logueado
+                @endauth
+              </span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="{{ asset('adminlte/img/user1.png') }}" class="img-circle" alt="User Image">
 
-      <!-- Header Navbar -->
-      <nav class="navbar navbar-static-top" role="navigation">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-          <span class="sr-only">Toggle navigation</span>
-        </a>
-        <!-- Navbar Right Menu -->
-        <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- User Account Menu -->
-            <li class="dropdown user user-menu">
-              <!-- Menu Toggle Button -->
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                <!-- The user image in the navbar-->
-                <img src="{{ asset('adminlte/img/user1.png') }}" class="user-image" alt="User Image">
-                <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                <span class="hidden-xs">
+                <p>
                   @auth
                   {{ auth()->user()->name}} 
                 @else
@@ -94,95 +107,20 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- sidebar: style can be found in sidebar.less -->
     <section class="sidebar">
 
-                  <p>
-                    @auth
-                    {{ auth()->user()->name}} 
-                  @else
-                    no esta logueado
-                  @endauth
-                  </p>
-                </li>
-                <!-- Menu Footer-->
-                <li class="user-footer">
-                  <div class="pull-right">
-                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                  </div>
-                </li>
-              </ul>
-            </li>
-          </ul>
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel">
+        <div class="pull-left image">
+          <img src="{{ asset('adminlte/img/user1.png') }}" class="img-circle" alt="User Image">
         </div>
-      </nav>
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-
-      <!-- sidebar: style can be found in sidebar.less -->
-      <section class="sidebar">
-
-        <!-- Sidebar user panel (optional) -->
-        <div class="user-panel">
-          <div class="pull-left image">
-            <img src="{{ asset('adminlte/img/user1.png') }}" class="img-circle" alt="User Image">
-          </div>
-          <div class="pull-left info">
-            <p>
-              @auth
-                {{ auth()->user()->name}}
-              @else
-                no esta logueado
-              @endauth
-            </p>
-          </div>
+        <div class="pull-left info">
+          <p>
+            @auth
+              {{ auth()->user()->name}}
+            @else
+              no esta logueado
+            @endauth
+          </p>
         </div>
-<<<<<<< HEAD
-        @auth
-          <!-- Sidebar Menu -->
-          <ul class="sidebar-menu" data-widget="tree">
-            <li class="treeview">
-              <a href="#"><i class="fa fa-building"></i></i> <span>Comercios</span>
-                <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                  </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="{{ route('commerces.index') }}">Ver comercios</a></li>
-                <li><a href="{{ route('commerces.create') }}">Crear comercio</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-check-square"></i> <span>Implementation</span>
-                <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                  </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="{{ route('implementation.index') }}">Ver implementation</a></li>
-                <li><a href="{{ route('implementation.create') }}">Crear implementation</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-link"></i> <span>Varios</span>
-                <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                  </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="{{ route('integrationtype.index') }}">Ver implementation</a></li>
-                <li><a href="{{ route('item.index') }}">Ver Items</a></li>
-              </ul>
-            </li>
-            <li class="treeview">
-              <a href="#"><i class="fa fa-link"></i> <span>Admin</span>
-                <span class="pull-right-container">
-                    <i class="fa fa-angle-left pull-right"></i>
-                  </span>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="{{ route('integrationtype.create') }}">Crear implementation</a></li>
-                <li><a href="{{ route('item.create') }}">Crear Item</a></li>
-                <li><a href="{{ route('indexcreateuser') }}">Crear Usuario</a></li>
-=======
       </div>
       @auth
         <!-- Sidebar Menu -->
@@ -231,118 +169,97 @@ scratch. This page gets rid of all links and provides the needed markup only.
               <li><a href="{{ route('item.create') }}">Crear Item</a></li>
               <li><a href="{{ route('indexcreateuser') }}">Crear Usuario</a></li>
               <li><a href="{{ route('getusers') }}">Ver Usuarios</a></li>
->>>>>>> 69c87037fda93ae3ab0410951136c7d9ab4e5906
 
-              </ul>
-            </li>
-          </ul>
-        @endauth  
-        <!-- /.sidebar-menu -->
-      </section>
-      <!-- /.sidebar -->
-    </aside>
-    <div class="content-wrapper">
+            </ul>
+          </li>
+        </ul>
+      @endauth  
+      <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+  <div class="content-wrapper">
+    
+      <!-- Content Header (Page header) -->
+      @yield('Page header')
       
-        <!-- Content Header (Page header) -->
-        @yield('Page header')
-        
-        <section class="content container-fluid">
-          @yield('content')
-        </section>
-        <!-- /.content -->
-    </div>
-    <!-- Main Footer -->
-    <footer class="main-footer">
-      <!-- To the right -->
-      <div class="pull-right hidden-xs">
-        Anything you want
-      </div>
-      <!-- Default to the left -->
-      <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
-    </footer>
-
-    <!-- Control Sidebar -->
-    <aside class="control-sidebar control-sidebar-dark">
-      <!-- Create the tabs -->
-      <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
-        <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
-        <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
-      </ul>
-      <!-- Tab panes -->
-      <div class="tab-content">
-        <!-- Home tab content -->
-        <div class="tab-pane active" id="control-sidebar-home-tab">
-          <h3 class="control-sidebar-heading">Recent Activity</h3>
-          <ul class="control-sidebar-menu">
-            <li>
-              <a href="javascript:;">
-                <i class="menu-icon fa fa-birthday-cake bg-red"></i>
-
-                <div class="menu-info">
-                  <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
-
-                  <p>Will be 23 on April 24th</p>
-                </div>
-              </a>
-            </li>
-          </ul>
-          <!-- /.control-sidebar-menu -->
-
-          <h3 class="control-sidebar-heading">Tasks Progress</h3>
-          <ul class="control-sidebar-menu">
-            <li>
-              <a href="javascript:;">
-                <h4 class="control-sidebar-subheading">
-                  Custom Template Design
-                  <span class="pull-right-container">
-                      <span class="label label-danger pull-right">70%</span>
-                    </span>
-                </h4>
-
-                <div class="progress progress-xxs">
-                  <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
-                </div>
-              </a>
-            </li>
-          </ul>
-          <!-- /.control-sidebar-menu -->
-
-        </div>
-        <!-- /.tab-pane -->
-        <!-- Stats tab content -->
-        <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
-        <!-- /.tab-pane -->
-        <!-- Settings tab content -->
-        <div class="tab-pane" id="control-sidebar-settings-tab">
-          <form method="post">
-            <h3 class="control-sidebar-heading">General Settings</h3>
-
-            <div class="form-group">
-              <label class="control-sidebar-subheading">
-                Report panel usage
-                <input type="checkbox" class="pull-right" checked>
-              </label>
-
-<<<<<<< HEAD
-              <p>
-                Some information about this general settings option
-              </p>
-            </div>
-            <!-- /.form-group -->
-          </form>
-        </div>
-        <!-- /.tab-pane -->
-      </div>
-    </aside>
-    <!-- /.control-sidebar -->
-    <!-- Add the sidebar's background. This div must be placed
-    immediately after the control sidebar -->
-    <div class="control-sidebar-bg"></div>
+      <section class="content container-fluid">
+        @yield('content')
+      </section>
+      <!-- /.content -->
   </div>
-@else
-  
-  
-=======
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Company</a>.</strong> All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Create the tabs -->
+    <ul class="nav nav-tabs nav-justified control-sidebar-tabs">
+      <li class="active"><a href="#control-sidebar-home-tab" data-toggle="tab"><i class="fa fa-home"></i></a></li>
+      <li><a href="#control-sidebar-settings-tab" data-toggle="tab"><i class="fa fa-gears"></i></a></li>
+    </ul>
+    <!-- Tab panes -->
+    <div class="tab-content">
+      <!-- Home tab content -->
+      <div class="tab-pane active" id="control-sidebar-home-tab">
+        <h3 class="control-sidebar-heading">Recent Activity</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:;">
+              <i class="menu-icon fa fa-birthday-cake bg-red"></i>
+
+              <div class="menu-info">
+                <h4 class="control-sidebar-subheading">Langdon's Birthday</h4>
+
+                <p>Will be 23 on April 24th</p>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+        <h3 class="control-sidebar-heading">Tasks Progress</h3>
+        <ul class="control-sidebar-menu">
+          <li>
+            <a href="javascript:;">
+              <h4 class="control-sidebar-subheading">
+                Custom Template Design
+                <span class="pull-right-container">
+                    <span class="label label-danger pull-right">70%</span>
+                  </span>
+              </h4>
+
+              <div class="progress progress-xxs">
+                <div class="progress-bar progress-bar-danger" style="width: 70%"></div>
+              </div>
+            </a>
+          </li>
+        </ul>
+        <!-- /.control-sidebar-menu -->
+
+      </div>
+      <!-- /.tab-pane -->
+      <!-- Stats tab content -->
+      <div class="tab-pane" id="control-sidebar-stats-tab">Stats Tab Content</div>
+      <!-- /.tab-pane -->
+      <!-- Settings tab content -->
+      <div class="tab-pane" id="control-sidebar-settings-tab">
+        <form method="post">
+          <h3 class="control-sidebar-heading">General Settings</h3>
+
+          <div class="form-group">
+            <label class="control-sidebar-subheading">
+              Report panel usage
+              <input type="checkbox" class="pull-right" checked>
+            </label>
+
             <p>
               Some information about this general settings option
             </p>
@@ -359,8 +276,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <div class="control-sidebar-bg"></div>
 </div>
 @else
-
->>>>>>> 69c87037fda93ae3ab0410951136c7d9ab4e5906
+<div id="topBar"> <a href ="#" onclick="load_home()"> HOME </a> </div>
+<div id ="content"> </div>
+  
   
 @endauth 
 <!-- ./wrapper -->
